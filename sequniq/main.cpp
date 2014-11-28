42ca6be442a8d29a8e17ac8616a5311e7999a8dc
Benjamin Schuster-Boeckler <bendaboots@gmx.de>
2014-11-28 14:07:21 +0000

    Initial commit

---
+++ sequniq/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sequniq
+//
+//  Created by Benjamin Schuster-Boeckler on 28/11/2014.
+//  Copyright (c) 2014 Benjamin Schuster-Böckler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
